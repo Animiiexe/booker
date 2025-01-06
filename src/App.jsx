d736a0840b15da741970bff861beea4de2f318a9
Animesh.MS <123883011+Animiiexe@users.noreply.github.com>
2025-01-06 14:41:07 +0000

    render all components

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import Form from "./Form";
+
+function App() {
+  const [books, setBooks] = useState([
+    {
+      id: crypto.randomUUID(),
+      text: "The Great Gatsby",
+    },
+    {
+      id: crypto.randomUUID(),
+      text: 1984,
+    },
+  ]);
+
+  const updateBooks = (book) => {
+    const newBook = {
+      id: crypto.randomUUID(),
+      text: book,
+    };
+    setBooks([newBook, ...books]);
+  };
+
+  const deleteBook = (id) => {
+    setBooks(books.filter((b) => b.id !== id));
+  };
+
+  return (
+    <>
+      <header>
+        <h1>Booker</h1>
+      </header>
+
+      <Form onSubmit={updateBooks} />
+
+      {books.map((book) => (
+        <pre key={book.id}>
+          {book.text}{" "}
+          <div >
+          <button onClick={() => deleteBook(book.id)}>Delete</button>
+          <button onClick={() => deleteBook(book.id)}>Delete</button>
+          <button onClick={() => deleteBook(book.id)}>Delete</button>
+          </div>
+        </pre>
+      ))}
+    </>
+  );
+}
+
+export default App;
